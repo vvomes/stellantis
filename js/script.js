8c9ace8802d924c16c6fba2002e1e1cae1130a0b
gdbianchii <gdbianchii@gmail.com>
2022-11-22 12:36:45 -0300

    Contate-nos, Time

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 
-// Duas funções,
-// Que tenham um if e else
 
+function contatar() {
+	if (document.getElementById('name').value == '')
+		return alert('Por favor, insira um nome')
+
+	if (document.getElementById('email').value == '' && document.getElementById('phoneNumber').value == '')
+		return alert('Por favor, insira um e-mail ou número de telefone')
+
+	if (document.getElementById('message').value == '')
+		return alert('Por favor insira uma mensagem')
+
+	alert('Mensagem enviada com sucesso. Obrgiado por entrar em contato conosco')
+
+}
